d2d4cbc33795c696d449de45ee63267ab6ec92ed
Paul Cantrell <cantrell@pobox.com>
2014-04-03 12:06:06 -0500

    Refactored common request behavior into ApiRoute

---
+++ src/main/java/edu/macalester/jokes/ApiRoute.java
@@ -0,0 +1,127 @@
+package edu.macalester.jokes;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.macalester.jokes.model.Joke;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import spark.Request;
+import spark.Response;
+import spark.ResponseTransformerRoute;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by paul on 2014/3/31.
+ */
+public abstract class ApiRoute extends ResponseTransformerRoute {
+    private static SessionFactory sessionFactory = createSessionFactory();
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    protected ApiRoute(String path) {
+        super(path);
+    }
+
+    @Override
+    public String render(Object model) {
+        return gson.toJson(model);
+    }
+
+    @Override
+    public final Object handle(Request req, Response res) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            Object resBody = doHandle(req, res, session);
+            tx.commit();
+
+            return resBody;
+
+        } catch(ConstraintViolationException e) {
+            tx.rollback();
+
+            // HTTP codes in the 4xx range mean that the client submitted a bad request.
+            // 400 is a good one for validation errors.
+            res.status(400);
+
+            Map<String,Object> resBody = new HashMap<String, Object>();
+            resBody.put("success", false);
+            resBody.put("error", e.getLocalizedMessage());
+
+            // Give the client field-by-field user-readable error messages
+            Map<String,String> errors = new HashMap<String, String>();
+            for(ConstraintViolation<?> violation : e.getConstraintViolations())
+                errors.put(violation.getPropertyPath().toString(), violation.getMessage());
+            resBody.put("validationErrors", errors);
+
+            return resBody;
+
+        } catch(Exception e) {
+            tx.rollback();
+
+            // HTTP codes in the 5xx range mean that something went wrong on the server,
+            // and it's not necessarily the client's fault.
+            res.status(500);
+
+            Map<String,Object> resBody = new HashMap<String, Object>();
+            resBody.put("success", false);
+            resBody.put("error", e.getLocalizedMessage());
+            return resBody;
+        }
+    }
+
+    protected abstract Object doHandle(Request req, Response res, Session session);
+
+
+    /**
+     * Standard Hibernate setup. Why do they not provide a single method instead of
+     * requiring all this boiler plate code? It is a mystery!
+     */
+    private static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        return configuration.buildSessionFactory(
+            new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/edu/macalester/jokes/JokeServer.java
+++ src/main/java/edu/macalester/jokes/JokeServer.java
@@ -1,20 +1,14 @@
 package edu.macalester.jokes;
 
-import com.google.gson.Gson;
 import edu.macalester.jokes.model.Joke;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
-import spark.Route;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.sql.Time;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,8 +17,6 @@ import static spark.Spark.*;
 public class JokeServer {
 
     public static void main(String[] args) {
-        final SessionFactory sessionFactory = createSessionFactory();
-
         // Heroku passes the port to listen on in the PORT environment variable.
         // The app won't start on Heroku unless we listen on this port.
         if(System.getenv("PORT") != null)
@@ -38,85 +30,34 @@ public class JokeServer {
             }
         });
 
-        get(new Route("/jokes") {
+        get(new ApiRoute("/jokes") {
             @Override
-            public Object handle(Request req, Response res) {
-                Session session = sessionFactory.openSession();
-                return new Gson().toJson(
-                    session.createQuery("from Joke").list());
+            public Object doHandle(Request req, Response res, Session session) {
+                return session.createQuery("from Joke").list();
             }
         });
 
-        get(new Route("/jokes/:id") {
+        get(new ApiRoute("/jokes/:id") {
             @Override
-            public Object handle(Request req, Response res) {
-                Session session = sessionFactory.openSession();
-                return new Gson().toJson(
-                    session.get(Joke.class, Long.parseLong(req.params("id"))));
+            public Object doHandle(Request req, Response res, Session session) {
+                return session.get(Joke.class, Long.parseLong(req.params("id")));
             }
         });
 
-        post(new Route("/jokes") {
+        post(new ApiRoute("/jokes") {
             @Override
-            public Object handle(Request req, Response res) {
-                Session session = sessionFactory.openSession();
-                Transaction tx = session.beginTransaction();
-                try {
-                    Joke joke = new Joke();
-                    joke.setSetup(req.queryParams("setup"));
-                    joke.setPunchline(req.queryParams("punchline"));
-                    session.save(joke);
-                    tx.commit();
+            public Object doHandle(Request req, Response res, Session session) {
+                Joke joke = new Joke();
+                joke.setSetup(req.queryParams("setup"));
+                joke.setPunchline(req.queryParams("punchline"));
+                session.save(joke);
 
-                    Map<String,Object> resBody = new HashMap<String, Object>();
-                    resBody.put("success", true);
-                    resBody.put("joke", joke);
-                    return new Gson().toJson(resBody);
-
-                } catch(ConstraintViolationException e) {
-                    tx.rollback();
-
-                    // HTTP codes in the 4xx range mean that the client submitted a bad request.
-                    // 400 is a good one for validation errors.
-                    res.status(400);
-
-                    Map<String,Object> resBody = new HashMap<String, Object>();
-                    resBody.put("success", false);
-                    resBody.put("error", e.getLocalizedMessage());
-
-                    // Give the client field-by-field user-readable error messages
-                    Map<String,String> errors = new HashMap<String, String>();
-                    for(ConstraintViolation<?> violation : e.getConstraintViolations())
-                        errors.put(violation.getPropertyPath().toString(), violation.getMessage());
-                    resBody.put("validationErrors", errors);
-
-                    return new Gson().toJson(resBody);
-
-                } catch(Exception e) {
-                    tx.rollback();
-
-                    // HTTP codes in the 5xx range mean that something went wrong on the server,
-                    // and it's not necessarily the client's fault.
-                    res.status(500);
-
-                    Map<String,Object> resBody = new HashMap<String, Object>();
-                    resBody.put("success", false);
-                    resBody.put("error", e.getLocalizedMessage());
-                    return new Gson().toJson(resBody);
-                }
+                Map<String,Object> resBody = new HashMap<String, Object>();
+                resBody.put("success", true);
+                resBody.put("joke", joke);
+                return resBody;
             }
         });
     }
 
-    /**
-     * Standard Hibernate setup. Why do they not provide a single method instead of
-     * requiring all this boiler plate code? It is a mystery!
-     */
-    private static SessionFactory createSessionFactory() {
-        Configuration configuration = new Configuration().configure();
-        return configuration.buildSessionFactory(
-                new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties())
-                        .build());
-    }
 }
